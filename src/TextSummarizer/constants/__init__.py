d02d7dc2cf46f2a646600dd96acc51d5c1840d7e
SatyaKotla <satyakrishna.kotla@gmail.com>
2024-06-07 00:43:42 -0400

    Changes in requirements and config

---
+++ src/TextSummarizer/constants/__init__.py
@@ -0,0 +1,7 @@
+#creating constants that to be used throughout the code
+#we are creating constants for the file paths config.yaml and params.yaml
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path('config/config.yaml')
+PARAMS_FILE_PATH = Path('params.yaml')
